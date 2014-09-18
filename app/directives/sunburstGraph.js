a930deaced52a686309e7badd408d75b055c1b49
satishn92 <satish@obelisksolutions.in>
2014-09-18 21:32:42 +0530

    Added Details and data

--- app/directives/sunburstGraph.js
+++ app/directives/sunburstGraph.js
@@ -8,9 +8,14 @@ angular.module('app.directives', [])
     },
     templateUrl: 'templates/sunburstGraphTemplate.html',
     link: function($scope,$element,$attr) {
+      // console.log($scope.data.children[0]);
+      // console.log(color1);
       var width = 960,
           height = 700,
           radius = Math.min(width, height) / 2,
+          detailedHtml = "",
+          totalBarLength = 0,
+          barLength = 0,
           color1 = d3.scale.category10(),
           color2 = d3.scale.category20(), 
           color3 = d3.scale.category20b(),
@@ -52,13 +57,12 @@ angular.module('app.directives', [])
             }
           })
           .style("fill-rule", "evenodd")
-          // .on("click", click)
           .on("mouseover", mouseover)
           .on("mouseleave", mouseleave)
           .on("mousemove", mousemove)
           .each(stash);
 
-      d3.selectAll("input").on("change", function change() {
+      d3.selectAll("input.valueBox").on("change", function change() {
         var value = this.value === "count"
             ? function() { return 1; }
             : function(d) { 
@@ -71,6 +75,15 @@ angular.module('app.directives', [])
           .transition()
             .duration(1500)
             .attrTween("d", arcTween);
+
+        showDetails(this.value,$scope.data.children);
+
+        d3.selectAll("a.view-data")
+          // .data($scope.data.children)
+          .on("click", showData);
+
+        d3.selectAll("a.view-children")
+            .on("click", showData);
       });
 
       var tooltip = d3.select("body").append("div")
@@ -85,6 +98,25 @@ angular.module('app.directives', [])
           .style("height", "400px")
           // .style("background-color", "red")
           .style("color", "white");
+
+      var Details = d3.select("body").append("div")
+          .attr("class", "Details")
+          .html(detailedHtml);
+
+      showDetails("count",$scope.data.children);
+
+      var Data = d3.select("body").append("div")
+          .attr("class", "Data");
+
+      d3.selectAll("a.view-data")
+          // .data($scope.data.children)
+          .on("click", showData);
+
+      d3.selectAll("a.view-children")
+            .on("click", showData);
+
+      d3.selectAll("a.view-previous")
+            .on("click", showData);
 
       // Stash the old values for transition.
       function stash(d) {
@@ -162,6 +194,40 @@ angular.module('app.directives', [])
         tooltip.style("left", (d3.event.pageX) + "px").style("top", (d3.event.pageY) + "px");
       }
 
+      function showDetails(value,elements) {
+
+        detailedHtml = "<h1 style='font-size: 14px; font-weight: 800;'>Distribution by Ratings</h1><hr class='details'>";
+        totalBarLength = 0;
+        var percentage = 0;
+
+        for(var root in elements) {
+          if(value === "count") {
+            barLength = (elements[root].children.length * 6);
+          } else {
+            barLength = (elements[root].value/2000);
+          }
+          totalBarLength += barLength;
+        }
+
+        for(var root in elements) {
+          if(value === "count") {
+            barLength = (elements[root].children.length * 6);
+          } else {
+            barLength = (elements[root].value/2000);
+          }
+          percentage = ((barLength/totalBarLength) * 100).toFixed(2);
+          detailedHtml += "<div class='row'><div class='col-md-2'><p class='details'>" + elements[root].name + "(" + elements[root].children.length + ")</p></div><div class='col-md-6'><div class='details' style=' width: " + barLength + "px; background-color:" +  color1(elements[root].name) + ";'></div></div><div class='col-md-2'><p class='details'>" + percentage + " %</p></div><div class='col-md-2'><a class='view-data' style='cursor: pointer;' >view</a></div></div><hr class='details'>";
+        }
+
+
+
+        Details.html(detailedHtml).transition().duration(3000);
+
+        d3.selectAll("a.view-data")
+          .data(elements);
+
+      }
+
       // function click(d) {
       //   d3.event.stopPropagation();
       //   console.log("Hi");
@@ -177,6 +243,41 @@ angular.module('app.directives', [])
           current = current.parent;
         }
         return path;
+      }
+
+      function showData(d) {
+        console.log(d);
+
+        var childrenHtml = "",
+            previousHtml = "";
+
+        if(d.parent.parent) {
+          previousHtml = "<p style=' margin: 0;'><a class='view-previous' style='cursor: pointer;'>Previous</a></p>";
+        } else {
+          previousHtml = "<p style=' margin: 0;'><a class='view-previous' style='cursor: pointer; display: none;' >Previous</a></p>";
+        }
+
+
+        var dataHtml = "<div class='row'><div class='col-md-10'><h1 style='font-size: 14px; font-weight: 800; margin-top: 0;'>Viewing children with parent: " + d.name + "</h1></div><div class='col-md-2'>" + previousHtml + "</div></div><hr class='details'>";
+        
+        
+        for(var element in d.children) {
+          if(d.children[element].children) {
+            childrenHtml = "<a class='view-children' style='cursor: pointer;' >Children</a>";
+          } else {
+            childrenHtml = "<a class='view-children' style='cursor: pointer; display: none;' >Children</a>";
+          }
+          dataHtml += "<div class='row'><div class='col-md-10'><p style='color: #CC6600'>" + d.children[element].name + "</p></div><div class='col-md-2'>" + childrenHtml + "</div></div><hr class='details'>";
+        }
+
+        Data.html(dataHtml);
+
+        d3.selectAll("a.view-previous")
+            .on("click", function() { showData(d.parent)});
+
+        d3.selectAll("a.view-children")
+            .data(d.children)
+            .on("click", showData);
       }
 
       d3.select(self.frameElement).style("height", height + "px");
